f5a30560a7d655845da114aa288892c9d7b056d6
ujwalp15 <ujwalp09@gmail.com>
2017-07-07 20:01:38 +0530

    logreg: initial script

---
+++ logreg.py
@@ -0,0 +1,15 @@
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+
+dataset = datasets.load_iris()
+
+model = LogisticRegression()
+model.fit(dataset.data, dataset.target)
+print(model)
+
+expected = dataset.target
+predicted = model.predict(dataset.data)
+
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
